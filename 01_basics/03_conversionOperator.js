188f5b9ed37df75b36047edadb23f3f768ed0e1c
Ashu pateriya <107784054+ashuu7089@users.noreply.github.com>
2023-09-04 11:24:07 +0000

    ConversionOperator

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,25 @@
+let score = "255fddfd";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "43" =>33
+//"dfd32" => NaN
+// true =>1, false => 0
+
+let isLoggedIn = 1
+let isBoolean = Boolean(isLoggedIn)
+console.log(isBoolean)
+
+//1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let stringValue = 33;
+let isStringValue = Boolean(stringValue)
+
+console.log(stringValue)
+console.log(typeof isStringValue)
